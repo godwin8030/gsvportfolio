153491c015e3f7cf654014d0994d3cafdaf18cff
Godwin <godwinshibu01@gmail.com>
2025-04-25 06:28:01 +0530

    Updated

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,11 @@ import {FaReact, FaJava, FaAws} from 'react-icons/fa';
 import {DiNodejs} from 'react-icons/di';
 import {TbBrandNextjs} from 'react-icons/tb';
 import {SiJavascript, SiTailwindcss} from 'react-icons/si';
+import { SiDjango } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { FaPython } from "react-icons/fa";
 import {BsFillSunFill} from 'react-icons/bs';
+import { SiFlask } from "react-icons/si";
 import {FaSass} from 'react-icons/fa';
 import {FaHtml5} from 'react-icons/fa';
 import {SiVite} from 'react-icons/si';
@@ -46,14 +50,14 @@ export default function Home() {
                 <BsFillSunFill style={{ color: 'white', display: darkMode ? 'inline-block' : 'none' }} />
               </li>
               <li className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300'>
-                <a className='bg-gradient-to-t from-purple-900 to-purple-500 text-white px-4 py-2 rounded-md ml-8 font-poppins hover:bg-gradient-to-b' href="https://drive.google.com/file/d/1vgAcXqIFL968_oAlYwM_eKLUs2OXADSM/view?usp=sharing">Resume</a>
+                <a className='bg-gradient-to-t from-purple-900 to-purple-500 text-white px-4 py-2 rounded-md ml-8 font-poppins hover:bg-gradient-to-b' href="https://drive.google.com/file/d/1mxqlTUlFFb98n54UmSZfzw2-XdpDFoxv/view?usp=sharing">Resume</a>
               </li>
             </ul>
           </nav>
           <div className='text-center px-13 py-10 lg:px-9'>
             <h2 className='text-5xl font-worksans font-semibold py-2 shadow-inner rounded-3xl shadow-gray-300 text-purple-900 md:text-6xl dark:text-purple-400 dark:shadow-black'>Godwin Shibu</h2>
-            <h3 className='text-l py-3 font-poppins text-gray-600 md:text-2xl dark:text-white' >Web Developer and 3D Designer.</h3>
-            <p className='text-sm py-5 text-justify font-poppins font-medium text-gray-700 md:text-lg max-w-xl mx-auto dark:text-white'>budding web developer. 3D model designer. working with computers since a kid. love the challenge of creating something new. always eager to learn new technologies.
+            <h3 className='text-l py-3 font-poppins text-gray-600 md:text-2xl dark:text-white' >Analyst & Web Developer</h3>
+            <p className='text-sm py-5 text-justify font-poppins font-medium text-gray-700 md:text-lg max-w-xl mx-auto dark:text-white'>budding web developer. analyst. 3D model designer. working with computers since a kid. love the challenge of creating something new. always eager to learn new technologies.
             </p>
             <div className='text-5xl flex justify-center flex-wrap gap-6 px-5 py-5 text-purple-900 shadow-xl rounded-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-300 md:gap-20 hover:shadow-purple-400 dark:text-purple-400 dark:shadow-black hover:dark:shadow-purple-500'>
               <a className='hover:text-blue-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200' href="https://www.linkedin.com/in/godwinshibu/"><AiFillLinkedin/></a>
@@ -72,12 +76,12 @@ export default function Home() {
             <h3 className='text-3xl pt-20 pb-6 font-poppins font-bold text-gray-700 text-center dark:text-white'>Skills</h3>
             <div className='text-6xl flex justify-center flex-wrap gap-10 p-4 shadow-2xl rounded-3xl text-purple-900 hover:shadow-purple-400 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 dark:text-purple-400 dark:shadow-black hover:dark:shadow-purple-500'>
                 <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-yellow-500'><SiJavascript/><span className='text-sm text-center flex justify-center'>Javascript</span></div>
-                <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-orange-500'><FaHtml5/><span className='text-sm text-center flex justify-center'>HTML</span></div>
-                <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-pink-500 dark:text-pink-400'><FaSass/><span className='text-sm text-center flex justify-center'>SaSS</span></div>
+                <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-orange-500'><FaPython/><span className='text-sm text-center flex justify-center'>Python</span></div>
+                <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-pink-500 dark:text-pink-400'><BiLogoPostgresql/><span className='text-sm text-center flex justify-center'>PostgreSQL</span></div>
                 <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-blue-500 dark:text-blue-400'><FaReact/><span className='text-sm text-center flex justify-center'>React</span></div>
-                <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-red-400 dark:text-red-400'><SiVite/><span className='text-sm text-center flex justify-center'>Vite</span></div>
+                <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-red-400 dark:text-red-400'><SiDjango/><span className='text-sm text-center flex justify-center'>Django</span></div>
                 <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-green-500'><DiNodejs/><span className='text-sm text-center flex justify-center'>Nodejs</span></div>
-                <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-gray-400 dark:hover:text-white'><TbBrandNextjs/><span className='text-sm text-center flex justify-center'>Nextjs</span></div>
+                <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-gray-400 dark:hover:text-white'><SiFlask/><span className='text-sm text-center flex justify-center'>Flask</span></div>
                 <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-blue-500'><SiTailwindcss/><span className='text-sm text-center flex justify-center'>Tailwind</span></div>
                 <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-red-500'><FaJava/><span className='text-sm text-center flex justify-center'>Java</span></div>
                 <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-200 text-yellow-500'><FaAws/><span className='text-sm text-center flex justify-center'>AWS</span></div>
