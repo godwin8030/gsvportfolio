885a5f162e361b399f8841ae5877b4c95899cc01
Godwin <godwinshibu01@gmail.com>
2025-04-26 12:13:07 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
             </ul>
           </nav>
           <div className='text-center px-13 py-10 lg:px-9'>
-            <h2 className='text-5xl font-worksans font-semibold py-2 shadow-inner rounded-3xl shadow-gray-300 text-purple-900 md:text-6xl dark:text-purple-400 dark:shadow-black'>Godwin Shibu</h2>
+            <h2 className='text-5xl font-worksans font-semibold py-2 shadow-inner rounded-3xl shadow-gray-300 text-purple-900 md:text-6xl dark:text-white-400 dark:shadow-black'>Godwin Shibu</h2>
             <h3 className='text-l py-3 font-poppins text-gray-600 md:text-2xl dark:text-white' >analyst & web dev</h3>
             <p className='text-sm py-5 text-justify font-poppins font-medium text-gray-700 md:text-lg max-w-xl mx-auto dark:text-white'>web dev. analyst. 3D model designer. working with computers since a kid. love the challenge of creating new stuff. always eager to learn new tech.
             </p>
