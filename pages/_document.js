7ab923d624fa68616b473f6399c1a3d484f39319
Godwin <godwinshibu01@gmail.com>
2025-07-22 22:44:46 +0530

    gs widget

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script id="gs-sdk" src="https://web-widget.gupshup.io/v3/demo/static/js/sdk.js" appId="fb50ed50-9384-40c1-a6e3-d51607e783c6" ref="ar"></script>
       </body>
     </Html>
   )
